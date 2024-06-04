5bf0c162b9b25d480736ea03735cc7bd2cad7a65
tranh0anghuan <121786621+tranh0anghuan@users.noreply.github.com>
2024-06-04 13:29:36 +0700

    add navbar and topbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './style.css'
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm px-5 py-3">
+        <a href="index.html" class="navbar-brand">
+            <h1 class="m-0 text-primary"><i class="fa fa-tooth me-2"></i>DentCare</h1>
+        </a>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <div class="dropdown-menu m-0">
+                        <a href="price.html" class="dropdown-item">Service</a>
+                        <a href="team.html" class="dropdown-item">Our Dentist</a>
+                        <a href="appointment.html" class="dropdown-item">Appointment</a>
+                        <a href="record.html" class="dropdown-item">Record</a>
+
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+                <a href="schedule.html" class="nav-item nav-link">Schedule</a>
+
+            </div>
+
+            <form action="">
+                <input type="text" class="nav-input"/>
+                <button type="button" class="btn text-dark" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fa-solid fa-magnifying-glass fs-5"></i></button>
+            </form>
+
+            <a href="login.html" class="btn btn-primary py-2 px-4 ms-3">Log In</a>
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
