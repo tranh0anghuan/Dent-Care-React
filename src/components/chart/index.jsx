98cca174920efc7823e5b7ea9692d1fe7b11e773
thanh-san <thanhcao643@gmail.com>
2024-07-17 16:51:02 +0700

    css phaanf chart

--- src/components/chart/index.jsx
+++ src/components/chart/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
 import api from '../../config/axios'
-import {getMonth, parse} from "date-fns"
+import { getMonth, parse } from "date-fns"
 function AdminChart() {
   const [data, setData] = useState([])
   const array = new Array(12).fill(0);
@@ -9,13 +9,13 @@ function AdminChart() {
     try {
       const response = await api.get("/appointment-patient");
       console.log(response.data);
-      const quantity  = response.data;
+      const quantity = response.data;
       console.log(quantity)
-      
+
       quantity.forEach(item => {
         const month = parse(item.date, 'yyyy-MM-dd', new Date());
         const monthIndex = getMonth(month); // Lấy tháng (giá trị từ 0 đến 11)
-      
+
         // Tăng giá trị tương ứng trong mảng array lên 1
         array[monthIndex]++;
       });
@@ -75,18 +75,25 @@ function AdminChart() {
       console.log(error)
     }
   }
-  useEffect(() => fetchData(), []);
-  return  <div style={{width: "auto", height: "auto", display: "flex", justifyContent: "center" , alignItems: "center"}}>
-      <LineChart width={1500} height={700} data={data}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="month" label="" / >
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotoneX" dataKey="quantity" stroke="#8884d8" strokeWidth={3}/>
-      </LineChart>
-    </div>  
+  useEffect(() => { fetchData() }, []);
+
+
+  return <div style={{ width: "auto", height: "auto", display: "flex", justifyContent: "center", alignItems: "center" }}>
+    <LineChart width={1500} height={700} data={data}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="month" />
+      <YAxis label={{
+        value: 'Quantity',
+        angle: -90,
+        position: 'insideLeft',
+        style: { fontSize: '20px', fontWeight: 'bold' }
+      }} />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="quantity" stroke="#1677ff" strokeWidth={3} />
+    </LineChart>
+  </div>
 }
 
 export default AdminChart
