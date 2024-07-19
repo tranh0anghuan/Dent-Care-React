66d8bd26df6a6d900acf9e2f2e6787c12d22472e
thanh-san <thanhcao643@gmail.com>
2024-07-20 00:37:41 +0700

    sap xep manage

--- src/pages/manager/index.jsx
+++ src/pages/manager/index.jsx
@@ -25,9 +25,9 @@ const Manager = () => {
     if (role === "manager") {
       setItems([
         { key: "room", label: "Room", icon: <PlusOutlined /> },
+        { key: "manager-dentist", label: "Manage Dentist", icon: <PlusOutlined />},
         { key: "managerappointment", label: "View Appointment", icon: <ProfileOutlined /> },
         { key: "manager-patient", label: "Manage Patient", icon: <ProfileOutlined /> },
-        { key: "manager-dentist", label: "Manage Dentist", icon: <PlusOutlined />},
         { key: "manager-service", label: "Manager Service", icon: <ProfileOutlined/>},
         { key: "manager-chart", label: "Manager Chart", icon: <ProfileOutlined/>},
       ]);
